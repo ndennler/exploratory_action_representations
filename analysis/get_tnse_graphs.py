7aab88b25ee16718e6bcd5ea31f81e58694aa20f
Dzeng401 <zengd@usc.edu>
2024-02-12 22:02:40 -0800

    pushing tnse graphs

--- analysis/get_tnse_graphs.py
+++ analysis/get_tnse_graphs.py
@@ -8,21 +8,32 @@ import plotly.express as px
 
 # refactor to use sysargs to take command line arguments later
 if __name__ == '__main__':
+    if len(sys.argv) != 5:
+        print("Not valid input")
+        exit()
     TASK_INDEX_MAPPING = {'idle': 0, 'searching': 1, 'has_information': 2, 'has_item': 3}
-    signal = "searching"
+    signal = sys.argv[1]
+    modality = sys.argv[2]
+    type = sys.argv[3]
+    file_name = sys.argv[4]
     all_data_csv = pd.read_csv('../data/all_data.csv')
     final_signals = pd.read_csv('../data/evaluation/concatenated_final_signals.csv')
     
     # get filtered version of each of the data
-    all_data_filtered = all_data_csv[all_data_csv['type'] == 'Video'][:-3]
+    # change
+    # use Video, Audio, or Movement
+    all_data_filtered = all_data_csv[all_data_csv['type'] == type][:-3]
     print(all_data_filtered['id'].max())
 
-    embeddings = np.load("../data/embeds/raw_taskembedding_visual_contrastive_128_task_embedder.pth.npy")
-    flattened_embeddings = embeddings[:,TASK_INDEX_MAPPING[signal],:]
+    #change
+    embeddings = np.load(f'../data/embeds/{file_name}')
+    flattened_embeddings = embeddings[:,TASK_INDEX_MAPPING[signal],:-3]
     print(embeddings.shape)
 
     # get final signal embeddings
-    selected_id = final_signals.query(f"signal=='{signal}'")['visual'].values
+    # change
+    # use visual,auditory, or kinesthetic
+    selected_id = final_signals.query(f"signal=='{signal}'")[modality].values
     selected_id = selected_id[selected_id > 0] 
     # first flatten with pca to 50 dimensions
     embeddings_pca = PCA(n_components = 50).fit_transform(flattened_embeddings)
@@ -44,19 +55,3 @@ if __name__ == '__main__':
     print(plot_df['color'].unique())
     fig = px.scatter(plot_df, x = "x", y = "y", color = "color", size = "size" , hover_data = ["tag", "id"], opacity = .5)
     fig.show()
-
-    # # Generate random colors
-    # colors = np.random.rand(867)
-    # # plot data
-    # plt.scatter(embeddings_2d[:, 0], embeddings_2d[:, 1], alpha = .2)
-    # plt.scatter(selected_embeddings[:, 0], selected_embeddings[:, 1], alpha = 1)
-    # plt.colorbar()
-    # plt.xlabel('X-embeddings')
-    # plt.ylabel('Y-embeddings')
-    # plt.title("Data for TSNE")
-    # plt.axis([-25, 25, -10, 30])
-    # plt.show()
-    # mappings = {}
-    # # create mappings
-    # for index, row in all_data_filtered.iterrows():
-    #     mappings[row['tags']].append(embeddings[row['id']])
